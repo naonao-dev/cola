cf8a2778fe4a24ae39fc866029bf0e45d4c31bab
naonao <1319144981@qq.com>
2025-05-17 11:29:52 +0800

    fix:删除文档

---
+++ tutorial/Common/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define CURRENT_PATH   "E:/demo/repo/cola"
+#define CONFIG_VERSION "0.0.1"
+#define CONFIG_VERSION_MAJOR 0
+#define CONFIG_VERSION_MINOR 0
+#define CONFIG_VERSION_ALTER 1
+#define GIT_COMMIT      "7ac1795"
+#define GIT_COMMIT_LONG "7ac17955ddb90086d1c0e934b0e8ef7e5da5bac7"
+#define GIT_COMMIT_DATE "20250429165014"
+#define GIT_BRANCH      "naonao_dev"
+#define GIT_TAG         "v0.1-27-g7ac1795"
+#define GIT_TAG_LONG    "v0.1-27-g7ac1795"
+#define GIT_CUSTOM      "v0.1-27-g7ac1795-7ac1795"
